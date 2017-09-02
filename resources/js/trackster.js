e02ab05c6692582db34d0d16ff19772d3d77d14e
Livia Rett <livia.rett@gmail.com>
2017-09-02 20:35:12 +0100

    all done

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,16 +1,25 @@
 const API_KEY = '14a704bdfee98fbff469d873c9910a6b';
 
 $('document').ready(function() {
-  
-   $('#searchbutton').click(function() {
     
+   $('.search-bar').keydown(function(event) {
+       if (event.which == 13) {
     Trackster.searchTracksByTitle($('.search-bar').val());
-       
+       };
+    });
+    
+   $('#searchbutton').click(function() {
+    Trackster.searchTracksByTitle($('.search-bar').val());
+    });
    });
     
-});
+
 
 var Trackster = {};
+
+function formatNumber(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+};
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
@@ -18,14 +27,18 @@ var Trackster = {};
 */
 
 Trackster.renderTracks = function(tracks) {
+    console.log(tracks);
+    $('.results').empty();
     for (var i = 0; i < tracks.length; i++) {
+    var numberToFormat = tracks[i].listeners;   
+    var mediumAlbumArt = tracks[i].image[1]['#text'];
     var trackRow = '<div class="row result"> \
                      <div class="col-xs-1"></div> \
                      <div class="col-xs-1 play"><a href="' + tracks[i].url + '"><i class="fa fa-play-circle-o" aria-hidden="true"></i></a></div> \
                      <div class="col-xs-1 rank">'+ (i + 1) + '</div> \
                      <div class="col-xs-3 song">'+ tracks[i].name + '</div> \
                      <div class="col-xs-2 artist">' + tracks[i].artist   + '</div> \
-                     <div class="col-xs-2 album"><img src="' + tracks[i].image[0] + '" title="xxx" /></div><div class="col-xs-1 popularity"></div> \
+                     <div class="col-xs-2 album"><img src="' + mediumAlbumArt + '" /></div><div class="col-xs-1 popularity">' + formatNumber(numberToFormat) + '</div> \
                      <div class="col-xs-1 length"></div> \
                     </div>';
         $('.results').append(trackRow);
@@ -39,7 +52,7 @@ Trackster.renderTracks = function(tracks) {
 Trackster.searchTracksByTitle = function(title) {
     
     return $.ajax({
-        url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+        url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track&track=' + title + '&api_key=' + API_KEY + '&format=json',
         success: function(response) { Trackster.renderTracks(response.results.trackmatches.track);
     } 
 });    
