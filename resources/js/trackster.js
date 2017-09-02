0006afc880b000716c818204e43cb4b9f434bd3a
Livia Rett <livia.rett@gmail.com>
2017-09-02 17:00:38 +0100

    main js done

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,7 +1,11 @@
+const API_KEY = '14a704bdfee98fbff469d873c9910a6b';
+
 $('document').ready(function() {
-    
+  
    $('#searchbutton').click(function() {
-       Trackster.searchTracksByTitle($('.searchbar').val());
+    
+    Trackster.searchTracksByTitle($('.search-bar').val());
+       
    });
     
 });
@@ -12,8 +16,20 @@ var Trackster = {};
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks. 
 */
-Trackster.renderTracks = function(tracks) {
 
+Trackster.renderTracks = function(tracks) {
+    for (var i = 0; i < tracks.length; i++) {
+    var trackRow = '<div class="row result"> \
+                     <div class="col-xs-1"></div> \
+                     <div class="col-xs-1 play"><a href="' + tracks[i].url + '"><i class="fa fa-play-circle-o" aria-hidden="true"></i></a></div> \
+                     <div class="col-xs-1 rank">'+ (i + 1) + '</div> \
+                     <div class="col-xs-3 song">'+ tracks[i].name + '</div> \
+                     <div class="col-xs-2 artist">' + tracks[i].artist   + '</div> \
+                     <div class="col-xs-2 album"><img src="' + tracks[i].image[0] + '" title="xxx" /></div><div class="col-xs-1 popularity"></div> \
+                     <div class="col-xs-1 length"></div> \
+                    </div>';
+        $('.results').append(trackRow);
+    };
 };
 
 /*
@@ -21,5 +37,10 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+    
+    return $.ajax({
+        url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+        success: function(response) { Trackster.renderTracks(response.results.trackmatches.track);
+    } 
+});    
 };
